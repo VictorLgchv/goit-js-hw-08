1046a61979c5ec4c8ea2325c92a4c0176f66b062
VictorLgchv <108681513+VictorLgchv@users.noreply.github.com>
2023-03-01 17:18:37 +0200

    completed first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galeryContainer = document.querySelector(".gallery");
+// 1. создать разметку через insertAdjacentHTML у Репеты есть об этом.
+const cardsMarkup = createImageCardsMurkup(galleryItems);
+
+galeryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createImageCardsMurkup(elem) {
+  return elem
+    .map(({ preview, original, description }) => {
+      return `
+<a class="gallery__item" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>`;
+    })
+    .join("");
+}
+// console.log(galleryItems);
+var lightbox = new SimpleLightbox(".gallery a", {
+  loop: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
